311e0de3d68a9ad1ed468253923f64da5b7125fc
JosielJunior <josiel-junior2@hotmail.com>
2022-08-13 21:46:31 -0300

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+# Food
+5.times do |i|
+    Expanse.create(
+        description: "expense #{i}", 
+        value: "#{ i*100 }", 
+        category: "Food",
+        date: "2022-0#{ i+1 }-10")
+end
+# Health
+5.times do |i|
+    Expanse.create(
+        description: "expense #{i+5}", 
+        value: "#{ i*100 }", 
+        category: "Health",
+        date: "2022-0#{ i+1 }-10")
+end
+# Education
+5.times do |i|
+    Expanse.create(
+        description: "expense #{i+10}", 
+        value: "#{ i*100 }", 
+        category: "Education",
+        date: "2022-0#{ i+1 }-10")
+end
+# Other
+5.times do |i|
+    Expanse.create(
+        description: "expense #{i+15}", 
+        value: "#{ i*100 }", 
+        category: "Other",
+        date: "2022-0#{ i+1 }-10")
+end
